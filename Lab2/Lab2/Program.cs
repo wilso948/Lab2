214e8d66bdd7fccfde051d39518b384005acaaf6
corey <cawilson948@gmail.com>
2019-04-13 18:15:51 -0400

    Lab 2 Push

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -5,8 +5,51 @@ namespace Lab2
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+            double length_1;
+            double width_1;
+            
+
+                string input10 = null;
+
+                do
+                {
+                    Console.WriteLine("Hello, Please enter the length:");
+                    length_1 = Double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Please enter the width:");
+                    width_1 = Double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("The area is  " + (length_1 * width_1));
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("The Perimeter is " + (2 * length_1 + 2 * width_1));
+
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Would you like to continue? (Yes/No)");
+
+                    input10 = Console.ReadLine();
+
+
+
+                } while (input10 != "No") ;
+                
+                   
+
+                
+
+                
+           
+
+          
         }
     }
 }
